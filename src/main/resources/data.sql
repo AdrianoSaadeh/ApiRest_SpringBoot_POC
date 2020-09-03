d7cc454b2a7537834f1ba564ddcf7e650b3b751f
AdrianoSaadeh <adriano.saadeh@sap.com>
2020-09-03 18:07:12 -0300

    subindo o codigo do projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('PS5', 11.00, 999.00);
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('Xbox', 4.00, 555.00);
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('Mega Drive', 3.00, 300.00);
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('NES', 7.00, 199.00);
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('N64', 3.00, 460.99);
