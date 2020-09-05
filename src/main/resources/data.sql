3387129ff2b64535965e9e77dc424c72ec3eb204
AdrianoSaadeh <adriano.saadeh@sap.com>
2020-09-05 14:39:02 -0300

    inserindo novos registros no banco de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('Xbox', 4.00, 555.00);
 INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('Mega Drive', 3.00, 300.00);
 INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('NES', 7.00, 199.00);
 INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('N64', 3.00, 460.99);
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('Game Boy', 1.00, 99.99);
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('Master System', 29.00, 101.00);
+INSERT INTO TB_PRODUTO(nome, quantidade, valor) VALUES('Neo Geo', 3.00, 460.99);
